f16abafa1f73c1347b73b82bbc6749cd816aa06f
Marak <marak.squires@gmail.com>
2013-12-22 14:24:40 +0100

    [refactor] Better namespaces for configuration data. Added coldstorage config data. Renamed ppcoin to peercoin.

--- config/dev.js
+++ config/dev.js
@@ -3,23 +3,50 @@ module['exports'] = {
   "frontend": {
     port: 8888
   },
-  "bitcoin": {
-    host: 'localhost',
-    port: 8333,
-    user: 'username',
-    pass: 'password'
+  "coldstorage": {
+    "bitcoin": [{ public_key: "148dBGs99rnFHSu3euZYziABse6Prs5L7J" }],
+    "dogecoin": [{ public_key: "DS6p3PRcPrECMRzX37H92PFoifnrj717ZB" }],
+    "peercoin": [{ public_key: "PRsyinCxhkJ5ib7cWXzqTTYXN7mwJybtA4" }]
   },
-  "ppcoin": {
-    host: 'localhost',
-    port: 9902,
-    user: 'username',
-    pass: 'password'
+  "deposits": {
+    "bitcoin": {
+      host: 'localhost',
+      port: 8333,
+      user: 'username',
+      pass: 'password',
+    },
+    "peercoin": {
+      host: 'localhost',
+      port: 9902,
+      user: 'username',
+      pass: 'password'
+    },
+    "dogecoin": {
+      host: 'localhost',
+      port: 9903,
+      user: 'username',
+      pass: 'password'
+    }
   },
-  "dogecoin": {
-    host: 'localhost',
-    port: 9903,
-    user: 'username',
-    pass: 'password'
+  "withdrawals": {
+    "bitcoin": {
+      host: 'localhost',
+      port: 9801,
+      user: 'username',
+      pass: 'password',
+    },
+    "peercoin": {
+      host: 'localhost',
+      port: 9802,
+      user: 'username',
+      pass: 'password'
+    },
+    "dogecoin": {
+      host: 'localhost',
+      port: 9803,
+      user: 'username',
+      pass: 'password'
+    }
   },
   "datasource": {
     type: "couchdb",
