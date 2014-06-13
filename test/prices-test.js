cc89e8a80e8104b556171e91d09610f16fee4f09
Marak <marak.squires@gmail.com>
2014-06-13 05:24:27 -0700

    [api] Added litecoin and dogecoin price feeds

--- test/prices-test.js
+++ test/prices-test.js
@@ -14,6 +14,8 @@ tap.test("attempt to fetch prices from live feeds", function (t) {
   t.equal(err, null);
   t.type(_prices, Object);
   t.type(_prices.prices.bitcoin, "string");
+  t.type(_prices.prices.dogecoin, "string");
+  t.type(_prices.prices.litecoin, "string");
   t.type(_prices.prices.peercoin, "string");
   t.end();
   });
@@ -26,6 +28,8 @@ tap.test("attempt to get last price from datasource", function (t) {
     }
     t.type(latestPrice, Object);
     t.type(latestPrice.prices.bitcoin, "string");
+    t.type(latestPrice.prices.dogecoin, "string");
+    t.type(latestPrice.prices.litecoin, "string");
     t.type(latestPrice.prices.peercoin, "string");
     t.equal(err, null);
     t.end();
